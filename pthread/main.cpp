597ccb6936f81dc402dd28450b2d45d724601cba
roflailxgod <vovavovava@mail.ru>
2019-04-04 16:46:47 +0300

    Improve code

--- pthread/main.cpp
+++ pthread/main.cpp
@@ -9,7 +9,7 @@
 
 #define MAXLINE 256
 
-std::unordered_set<int> fds;
+static std::unordered_set<int> fds;
 
 static void *connection_handler(void *);
 
@@ -27,7 +27,6 @@ int main()
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(54002);
-//    inet_pton(AF_INET, "0.0.0.0", &servaddr.sin_addr);
 
     if (bind(listenfd, (sockaddr*)&servaddr, sizeof(servaddr)) == -1)
     {
@@ -45,8 +44,7 @@ int main()
 
     for (;;)
     {
-        connfd = accept(listenfd, NULL, 0);
-        if (connfd == -1)
+        if ((connfd = accept(listenfd, nullptr, nullptr)) == -1)
         {
             std::cerr << "Connect error";
             exit(1);
@@ -56,13 +54,11 @@ int main()
             fds.insert(connfd);
         }
 
-        int ret;
-        if ((ret = pthread_create(&thread, NULL, &connection_handler, (void*)&connfd)) < 0)
+        if (pthread_create(&thread, nullptr, &connection_handler, (void*)&connfd) < 0)
         {
             std::cerr << "Thread creation error";
             exit(1);
         }
-        std::cout << ret;
     }
 }
 
@@ -72,7 +68,7 @@ size_t writen(int sockfd, const char *buf, size_t count)
     size_t n;
     ssize_t rc;
 
-    if (buf == NULL)
+    if (buf == nullptr)
     {
         errno = EFAULT;
         std::cerr << "writen() error";
@@ -110,7 +106,7 @@ static void *connection_handler(void *arg)
     int sockfd = *((int *)arg);
     for (;;)
     {
-        if ((n = read(sockfd, buf, MAXLINE)) == 0)
+        if ((n = read(sockfd, buf, MAXLINE)) <= 0)
             break;
         for (const auto&elem: fds)
         {
@@ -134,5 +130,5 @@ static void *connection_handler(void *arg)
         fds.erase(sockfd);
     }
 
-    return (NULL);
+    return nullptr;
 }
